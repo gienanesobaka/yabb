e2ce604871935351083da087b9e33a85f3e3b2c8
Igor Gorbunov <igormbox@gmail.com>
2015-08-04 15:26:38 +0300

    reimplemented CategoryNode.

--- code/frontend/src/main/scala/gie/yabb/category/category-editor-controller.scala
+++ code/frontend/src/main/scala/gie/yabb/category/category-editor-controller.scala
@@ -1,7 +1,10 @@
 package gie.yabb
 
+import java.util.UUID
+
 import biz.enef.angulate.{Scope, Controller}
 import slogging.LazyLogging
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.scalajs.js.Dynamic.literal
 import scala.scalajs.js.{Array}
@@ -17,21 +20,20 @@ abstract class Item[SelfT <: Item[_]](p_children:Seq[Item[SelfT]] = Seq()){
   var label:String
   var children:js.Array[Item[SelfT]] = p_children.toJSArray
 
+  def asSelf = this.asInstanceOf[SelfT]
+
 }
 
-class CategoryNode(var cat:Category, children:Seq[CategoryNode]=Seq()) extends Item[CategoryNode](children){
-  override def label:String = cat.name
-  override def label_=(name:String): Unit ={
-    cat = cat.copy(name = name)
-  }
+package CategoryNodeState {
+  sealed trait CategoryNodeState
+  case class Clean() extends CategoryNodeState
+  case class Dirty() extends CategoryNodeState
+  case class New() extends CategoryNodeState
+}
 
-  def isDirty = cat.id==CategoryMagic.catIdDirty
-  def isNew = cat.id==CategoryMagic.catIdNew
-  def id = {
-    assume(cat.id>=0)
-    cat.id
-  }
+class CategoryNode(var label:String, var state:CategoryNodeState.CategoryNodeState, nodeId:Option[Long] = None, children:Seq[CategoryNode]=Seq()) extends Item[CategoryNode](children){
 
+  val id:Either[UUID, Long] = nodeId.toRight(gie.UUID())
   val deleteQueue = ArrayBuffer[CategoryNode]()
 }
 
@@ -54,7 +56,7 @@ class CategoryEditorController($scope:Scope) extends Controller with LazyLogging
     nodeLabel = p.label
   }
 
-  private val rootNode = new Node(Category(0, None, "[ROOT]"))
+  private val rootNode = new Node("[ROOT]", CategoryNodeState.Clean())
 
   var data:js.Array[Node] = Array(rootNode).toJSArray
 
@@ -71,23 +73,25 @@ class CategoryEditorController($scope:Scope) extends Controller with LazyLogging
       .filter{ case(node, parent) => !node.children.find(_.label==newNodeName).isDefined }
       .foreach{ case(node,parent)=>
 
-      Category(CategoryMagic.catIdNew, None, newNodeLabel) |>
-        (d=>new Node(d)) |>
-        (node.children.push(_))
+      node.children.push( new Node(newNodeLabel, CategoryNodeState.New()) )
 
     }
   }
 
   def rename(): Unit ={
-    import CategoryMagic._
 
     selected.filter{ case(node, parent)=>node ne rootNode }.fold{
       logger.debug("nothing to rename")
     }{ case(node, parentNode)=>
 
-        node.cat = node.cat.copy(id = if(node.cat.id==catIdNew) catIdNew else catIdDirty, name = nodeLabel)
+      node.label = nodeLabel
 
-        logger.debug(s"${node.cat}")
+      node.state match {
+        case CategoryNodeState.Clean() => node.state = CategoryNodeState.Dirty()
+        case CategoryNodeState.New() =>
+        case CategoryNodeState.Dirty() =>
+      }
+
     }
   }
 
@@ -100,14 +104,51 @@ class CategoryEditorController($scope:Scope) extends Controller with LazyLogging
       val idx = peers.indexOf(node); assume(idx != -1)
       peers.remove(idx)
 
-      if(node.isNew){
-        logger.debug(s"Node '${node.label}' is new, no need to record 'delete' operation.")
-      } else {
-        parentNode.deleteQueue += node
+      node.state match {
+        case CategoryNodeState.New() =>
+          logger.debug(s"Node '${node.label}' is a new, no need to record 'delete' operation.")
+        case _ =>
+          parentNode.deleteQueue += node
       }
 
       selected = None
     }
+  }
+
+
+  private def prepareCommandBuffer(): Unit ={
+
+    type ComplexNodeId = Option[Either[UUID, Long]]
+
+    val commandBuffer = new ArrayBuffer[CategoryCommand]()
+
+    def processChildrenOf(node: CategoryNode): Unit ={
+
+      val myId: ComplexNodeId = if(node eq rootNode) None else Some(node.id)
+
+      node.children.foreach(n=>processNode(n.asSelf, myId))
+    }
+
+    def processNode(node: CategoryNode, parentNodeId: ComplexNodeId): Unit ={
+
+      node.state match {
+        case CategoryNodeState.Clean() => logger.debug(s"node '${node.label}' is clean")
+        case CategoryNodeState.New()   => processNode_new(node, parentNodeId)
+        case CategoryNodeState.Dirty() => processNode_dirty(node, parentNodeId)
+      }
+
+    }
+
+    def processNode_new(node: CategoryNode, parentNodeId: ComplexNodeId): Unit ={
+      commandBuffer += CategoryCommand(CategoryMagic.command.create, name = Some(node.label) )
+    }
+
+    def processNode_dirty(node: CategoryNode, parentNodeId: ComplexNodeId): Unit ={
+
+    }
+
+    processChildrenOf(rootNode)
+
   }
 
 }
--- code/shared/gie/yabb/categories/categories.scala
+++ code/shared/gie/yabb/categories/categories.scala
@@ -6,8 +6,6 @@ case class Category(id: Long=0, parentId: Option[Long], name: String)
 
 
 object CategoryMagic {
-  val catIdDirty = -1
-  val catIdNew = -2
 
   object command {
     val noop   = 0
@@ -21,7 +19,7 @@ object CategoryMagic {
 
 
 
-case class CategoryCommand(command: Int, nodeId: Option[Long] = None, name: Option[String] = None)
+case class CategoryCommand(command: Int, nodeId: Option[Long] = None, parentId:Option[Either[UUID, Long]] = None, name: Option[String] = None)
 
 
 
